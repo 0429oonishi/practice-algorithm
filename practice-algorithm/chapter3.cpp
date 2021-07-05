bc08f55c2baf7bbe783eedc5166186b55f5b55dc
0429oonishi <oonishireon110@gmail.com>
2021-07-05 13:20:10 +0900

    chapter3実装

--- practice-algorithm/chapter3.cpp
+++ practice-algorithm/chapter3.cpp
@@ -5,4 +5,22 @@
 //  Created by 大西玲音 on 2021/06/18.
 //
 
-#include <stdio.h>
+#include "include/bits/stdc++.h"
+using namespace std;
+
+int main() {
+    int N, v;
+    cin >> N >> v;
+    vector<int> a(N);
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+    bool exist = false;
+    for (int i = 0; i < N; i++) {
+        if (a[i] == v) {
+            exist = true;
+        }
+    }
+    if (exist) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
